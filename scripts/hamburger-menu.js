cc660473d209cbd3d85db2cce1aba1bbe8661a00
Lord-Nickelbottoms <nizaam.h@icloud.com>
2024-10-15 22:40:02 +0200

    Changed layout and added media queries for mobile responsive behaviour.
    Added script file

---
+++ scripts/hamburger-menu.js
@@ -0,0 +1,11 @@
+function hamburgerMenuSwitch() {
+	var element = document.getElementById( "nav-links" )
+	
+	if ( element.style.display == "block" )
+	{
+		element.style.display = "none"
+	} else
+	{
+		element.style.display = "block"
+	}
+}
