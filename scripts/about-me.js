97ca2d687a808013c245957fc950aae7b0e9e3f8
Nizaam Haffejee <nizaam.h@icloud.com>
2024-10-17 11:39:28 +0200

    Added js file to change image when hover
    Added relevant js attributes in html file

---
+++ scripts/about-me.js
@@ -0,0 +1,7 @@
+window.onload = (event) => {
+    console.log("All files downloaded and initialised.")
+}
+
+function changeImage(image) {
+    document.getElementById("skill-showcaser-image").src = image
+}
