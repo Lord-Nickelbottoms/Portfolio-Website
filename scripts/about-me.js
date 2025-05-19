5c3f77c70ef7c7af1f74c2a8fb489db630229a17
Nizaam Haffejee <nizaam.h@icloud.com>
2025-05-19 10:21:04 +0200

    added function to ensure that only the styling class for COBOL applies to the image and the others don't

--- scripts/about-me.js
+++ scripts/about-me.js
@@ -5,3 +5,12 @@ window.onload = (event) => {
 function changeImage(image) {
     document.getElementById("skill-showcaser-image").src = image
 }
+
+function setImageStyle(styleName) {
+    // if (document.getElementById("skill-showcaser-image").className = styleName)
+    if (styleName !== " ") {
+        document.getElementById("skill-showcaser-image").className = styleName
+    } else {
+        document.getElementById("skill-showcaser-image").className = ""
+    }
+}
